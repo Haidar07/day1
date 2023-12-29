696dd870cdc53e18a59c2888cb2406bf2b73ecd1
Haidar07 <haidarjf07@gmail.com>
2023-12-29 13:47:24 +0700

    membuat ketika kita mengisikan kolom contact me, data yang diiskan otomatis ditangkap dan dibawa ke gmail/apk mail bawaan

---
+++ js/contactme.js
@@ -0,0 +1,30 @@
+function submitForm() {
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let phoneNumber = document.getElementById('phone-number').value;
+    let subject = document.getElementById('subject').value;
+    let message = document.getElementById('tm').value;
+
+    // Email address yang ingin kita gunakan
+    const emailAddress = 'haidarjf07@gmail.com';
+
+    // Subject email
+    const emailSubject = subject;
+    
+    // Pesan email 
+    const emailBody = 'Nice to meet you sir.' + '\n' +
+                      'My Name: ' + name + '\n' + 
+                      'My Phone: ' + phoneNumber + '\n' + '\n' + '\n' +
+                      'Message:\n' + message;
+
+    // Membuat URL Gmail khusus dengan parameter
+    const gmailUrl = 'https://mail.google.com/mail/u/0/?view=cm&fs=1&to=' + encodeURIComponent(emailAddress) +
+                    '&su=' + encodeURIComponent(emailSubject) +
+                    '&body=' + encodeURIComponent(emailBody);
+
+    // Membuka Gmail di tab atau jendela baru
+    window.open(gmailUrl, '_blank');
+
+    // Membuka program email default dengan nilai yang diisi
+    // window.location.href = mailtoUrl;
+}
